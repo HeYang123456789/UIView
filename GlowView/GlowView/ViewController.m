4adc2e0cfcdd9c3487a2c2ca47d5e603956b728e
HeYang123456789 <1554566348@qq.com>
2016-01-31 05:15:41 +0800

    add picture

--- GlowView/GlowView/ViewController.m
+++ GlowView/GlowView/ViewController.m
@@ -7,12 +7,13 @@
 //
 
 #import "ViewController.h"
+
 #import "GlowLayer.h"
 
 @interface ViewController ()
 
 /** UILabel */
-@property (nonatomic,weak)UILabel *label;
+@property (nonatomic,weak)UIImageView *imageView;
 @end
 
 @implementation ViewController
@@ -27,37 +28,40 @@
         
         label.center    = self.view.center;
         [self.view addSubview:label];
-        self.label = label;
         
         [label addGlowLayerWithGlowColor:[UIColor cyanColor]];
         [label.glowLayer startGlowAnimation];
     }
-    {
-        UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(180, 250, 45, 75)];
-        imageView.image        = [UIImage imageNamed:@"demo"];
-        [self.view addSubview:imageView];
-        
-        [imageView addGlowLayerWithGlowColor:[UIColor cyanColor]];
-        
-        imageView.glowLayer.glowRadius        = @(2.f);
-        imageView.glowLayer.glowOpacity       = @(0.5f);
-        
-        imageView.glowLayer.glowDuration      = @(1.f);
-        imageView.glowLayer.hideDuration      = @(0.5f);
-        imageView.glowLayer.glowAnimationDuration = @(1.f);
-        
-        [imageView.glowLayer startGlowAnimation];
-    }
+
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(180, 250, 45, 75)];
+    imageView.image        = [UIImage imageNamed:@"demo"];
+    [self.view addSubview:imageView];
+    
+    // 1.添加GlowView  --> 这个方法是在UIView类别中拓展的接口
+    [imageView addGlowLayerWithGlowColor:[UIColor cyanColor]];
+    
+    // 2.以下属性以及方法(比如startGlowAnimation) --> 都是GlowLayer自定义类中的属性和方法
+    imageView.glowLayer.glowRadius        = @(2.f);
+    imageView.glowLayer.glowOpacity       = @(0.5f);
+    
+    imageView.glowLayer.glowDuration      = @(1.f);
+    imageView.glowLayer.hideDuration      = @(0.5f);
+    imageView.glowLayer.glowAnimationDuration = @(1.f);
+    
+    [imageView.glowLayer startGlowAnimation];
+    
+    self.imageView = imageView;
 }
 -(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
     static BOOL isTrue  = true;
     if (isTrue == true) {
         // 暂停辉光动画
-        [self.label.glowLayer pauseGlowAnimation];
+        [self.imageView.glowLayer pauseGlowAnimation];
     }else{
         // 重启辉光动画
-        [self.label.glowLayer reStareGlowAnimation];
+        [self.imageView.glowLayer reStareGlowAnimation];
     }
     isTrue = !isTrue;
 }
+
 @end
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-1、GlowView
+## 1、GlowView
 
 ![](http://chuantu.biz/t2/24/1454186379x-1376440148.gif)
+
+学习来自：[YouXianMing老师的GlowView](https://github.com/YouXianMing/GlowView)
+YouXianMing老师用的是类别，但是本人将其改写为继承自CALayer的普通类，然后仅仅用了很简单的类别将对外接口进行了简单的封装。所以外部调用起来也是同样的方便，可读性也是比较好的：
+
+![](http://chuantu.biz/t2/24/1454188449x-954497756.png)
+### 然后简单的使用示例：
+![](http://chuantu.biz/t2/24/1454188366x-1376440148.png)
